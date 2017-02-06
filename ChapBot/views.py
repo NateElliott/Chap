94073104dd09580817f10e25e05d23cd5978185c
Nate <nelliott.ca@gmail.com>
2017-02-06 18:15:57 -0400

    added sec key area

--- ChapBot/views.py
+++ ChapBot/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+from .helper import sec
+
 
 class ChapBotView(View):
 
@@ -26,11 +28,11 @@ class ChapBotView(View):
         for entry in message_payload['entry']:
             for message in entry['messaging']:
                 if 'message' in message:
-                    message_text = message['message']['text']
-                    message_sender_id = message['sender']['id']
 
-                    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='
-                    response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": recevied_message}})
-                    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},
-                                           data=response_msg)
+                    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(sec.load('fbkey'))
+                    response_msg = json.dumps({"recipient": {"id": message['sender']['id']}, "message": {"text": message['message']['text']}})
+                    status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+
+                    return HttpResponse('gtg')
+
 
