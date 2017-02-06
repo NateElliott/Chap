6d2ad58d85450f286e8c42bd206c638fad37c898
Nate <nelliott.ca@gmail.com>
2017-02-06 17:52:40 -0400

    init

--- ChapBot/views.py
+++ ChapBot/views.py
@@ -1,27 +1,9 @@
-from django.shortcuts import render
+import json, requests
+
 from django.views import View
 from django.http import HttpResponse
-
-
-
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-
-import json
-import requests
-
-# Create your views here.
-
-
-def post_facebook_message(fbid, recevied_message):
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAXUP705SWcBABV6CxKHG0Itm15YmZC4nEZC7lA1592qR1SPe8hk1srYfIEggSaIaNj5FBOQcjDPa1VPKoEzxP9wimVKcO6ob8CjHB2FfWpZCgzxPV65OJxcOkNL0HaA35NSS9tuQFNSfltmI6yFQ1deqGA2M09COPZBmHcyMwZDZD'
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
-    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-
-
-
-
-
 
 
 class ChapBotView(View):
@@ -30,16 +12,12 @@ class ChapBotView(View):
     def dispatch(self, request, *args, **kwargs):
         return super(ChapBotView, self).dispatch( request, *args, **kwargs)
 
-
     def get(self, request):
 
         if request.GET.get('hub.verify_token') == '4b4ac4dcf31c237ca31ad39cf719a664':
             return HttpResponse(request.GET['hub.challenge'])
-
         else:
             return HttpResponse('gtg')
-
-
 
     def post(self, request):
 
@@ -51,7 +29,8 @@ class ChapBotView(View):
                     message_text = message['message']['text']
                     message_sender_id = message['sender']['id']
 
-                    post_facebook_message(message_sender_id,message_text)
+                    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token='
+                    response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": recevied_message}})
+                    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},
+                                           data=response_msg)
 
-
-        return HttpResponse('gtg')
